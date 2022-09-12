bf4b411b83f852d6f7c02691f8c152e227512302
EpicPuppy613 <61771471+EpicPuppy613@users.noreply.github.com>
2022-09-12 08:31:57 -0700

    build pls

--- script.ts
+++ script.ts
@@ -1,2 +1,4 @@
 const canvas: HTMLCanvasElement = document.getElementById('game');
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
